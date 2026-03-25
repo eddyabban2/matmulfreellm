4339941f3749b2da9e189b5e2d2ad5c550e8b280
eddyabban2 <eddyabban2@gmail.com>
2026-03-25 15:23:01 -0700

    corrected script name and added new one

--- scripts/ncu_layernorm_profile.py
+++ scripts/ncu_layernorm_profile.py
@@ -1,4 +1,19 @@
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="profiles layer norm"
+)
+
+parser.add_argument(
+    "-p", 
+    "--profile",
+    default="detailed",
+    help="sets the sequence length of input tokens"
+)
+
+args = parser.parse_args()
+metric_profile = args.profile
 
 ncu_path = subprocess.check_output(["which", "ncu"]).decode('ascii').strip()
 print(f"Extracted ncu path: {ncu_path}")
@@ -10,7 +25,7 @@ command = [
     "--export", "/home/eabban/matmulfreellm/ncu_runs/batch10Iter10",
     "--force-overwrite",
     "--target-processes", "application-only",
-    "--set", "detailed",
+    "--set", metric_profile,
     "/home/eabban/matmulfreellm/layer_norm_only.py",
     "-b", "10",
     "-i", "1"
