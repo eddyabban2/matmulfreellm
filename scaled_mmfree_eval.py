0f0be89116e780717382cd12385ce2bb2f7123d6
eddyabban2 <eabban@ucsc.edu>
2026-07-28 15:27:30 -0700

    updated scaled mmfree eval

--- scaled_mmfree_eval.py
+++ scaled_mmfree_eval.py
@@ -14,7 +14,7 @@ from transformers import logging
 import argparse
 import csv
 from scaled_mmfree import create_scaled_mmfree
-from generate_csv import benchmark_generation, detailed_runtime_metrics, run_warmup
+from generate_csv import benchmark_generation, detailed_runtime_metrics, run_warmup, get_power_data
 
 parser = argparse.ArgumentParser(
     description="creates a csv file with benchmark results"
@@ -30,21 +30,21 @@ parser.add_argument(
 parser.add_argument(
     "-w", 
     "--weight_multiplier",
-    default=3.9375,
+    default=0.5,
     help="sets the number of we multiply the number of weights in each layer by"
 )
 
 parser.add_argument(
     "-l", 
     "--layers_multiplier",
-    default=2.5,
+    default=0.5,
     help="sets the number  we multiply the number of layers by"
 )
 
 parser.add_argument(
     "-v", 
     "--vocab_multiplier",
-    default=4,
+    default=2,
     help="sets the number  we multiply the number of layers by"
 )
 
@@ -169,7 +169,13 @@ def create_csv_data(
                 start_time = time.time()
                 detailed_runtime_metrics(model, batch_size, sequence_length, iters, max_new_tokens, row, model_name=model_name, use_dataset_prompts=False)
                 end_time = time.time()
-                print(f"\t\t\tPrefill and Decode Times completed in {end_time-start_time} sec")
+                print(f"\t\t\tCollected more detailed runtime metrics in {end_time-start_time} sec")
+
+                start_time = time.time()
+                get_power_data(model, batch_size, sequence_length, iters, max_new_tokens, row, model_name=model_name)
+                end_time = time.time()
+                print(f"\t\t\tCollected power data in {end_time-start_time} sec")
+
                 if(first_row):
                     csvwriter = csv.DictWriter(csvfile, row.keys())
                     csvwriter.writeheader()
